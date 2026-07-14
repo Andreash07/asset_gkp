c31c2e6c7423f446149f4d475227d2e9db190c03
Andreas Febrian I <44549277+Andreash07@users.noreply.github.com>
2026-07-14 20:50:01 +0700

    update dashboard

---
+++ application/views/home/dashboard.php
@@ -0,0 +1,169 @@
+<?php
+$this->load->view('layout/header');
+?>
+<div class="right_col" role="main">
+ 	<!-- top tiles -->
+  	<div class="row tile_count">
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+          <span class="count_top"><i class="fa fa-user"></i> Total Aset</span>
+          <div class="count">1.284</div>
+          <!--<span class="count_bottom"><i class="green">4% </i> From last Week</span>-->
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+          <span class="count_top"><i class="fa fa-clock-o"></i> Total Luas Tanah</span>
+          <div class="count">2.543.320 m²</div>
+          <!--<span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>-->
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+          <span class="count_top"><i class="fa fa-user"></i> Atas Nama Jemaat</span>
+          <div class="count green">2,500</div>
+          <!--<span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>-->
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+          <span class="count_top"><i class="fa fa-user"></i> Atas Nama Sinode</span>
+          <div class="count">4,567</div>
+          <!--<span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>-->
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+          <span class="count_top"><i class="fa fa-user"></i> Aset Bersertifikat</span>
+          <div class="count">2,315</div>
+          <!--<span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>-->
+        </div>
+        <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+          <span class="count_top"><i class="fa fa-user"></i> Belum Bersertifikat</span>
+          <div class="count">7,325</div>
+          <!--<span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>-->
+        </div>
+  	</div>
+  	<!-- /top tiles -->
+
+  	<div class="row">
+        <div class="col-md-4 col-sm-4 col-xs-12">
+      		<div class="x_panel tile fixed_height_320">
+	            <div class="x_title">
+		          	<h2>Status Legalitas</h2>
+		          	<ul class="nav navbar-right panel_toolbox">
+		                <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+		                </li>
+		                <li class="dropdown">
+		                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+		                  <ul class="dropdown-menu" role="menu">
+		                    <li><a href="#">Settings 1</a>
+		                    </li>
+		                    <li><a href="#">Settings 2</a>
+		                    </li>
+		                  </ul>
+		                </li>
+		                <li><a class="close-link"><i class="fa fa-close"></i></a>
+		                </li>
+		          	</ul>
+		          	<div class="clearfix"></div>
+	            </div>
+	            <div class="x_content">
+		          	<div class="widget_summary">
+		                <div class="w_left w_25">
+		                  <span>Sertipikat</span>
+		                </div>
+		                <div class="w_center w_55">
+		                  <div class="progress">
+		                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 66%;">
+		                      <span class="sr-only">60% Complete</span>
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="w_right w_20">
+		                  <span>123k</span>
+		                </div>
+		                <div class="clearfix"></div>
+	              	</div>
+
+	              	<div class="widget_summary">
+		                <div class="w_left w_25">
+		                  <span>AJB</span>
+		                </div>
+		                <div class="w_center w_55">
+		                  <div class="progress">
+		                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 45%;">
+		                      <span class="sr-only">60% Complete</span>
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="w_right w_20">
+		                  <span>53k</span>
+		                </div>
+		                <div class="clearfix"></div>
+	              	</div>
+	              	<div class="widget_summary">
+		                <div class="w_left w_25">
+		                  <span>Girik/Letter C/Persil</span>
+		                </div>
+		                <div class="w_center w_55">
+		                  <div class="progress">
+		                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">
+		                      <span class="sr-only">60% Complete</span>
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="w_right w_20">
+		                  <span>23k</span>
+		                </div>
+		                <div class="clearfix"></div>
+	              	</div>
+	              	<div class="widget_summary">
+		                <div class="w_left w_25">
+		                  <span>Eigendom Verponding</span>
+		                </div>
+		                <div class="w_center w_55">
+		                  <div class="progress">
+		                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 5%;">
+		                      <span class="sr-only">60% Complete</span>
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="w_right w_20">
+		                  <span>3k</span>
+		                </div>
+		                <div class="clearfix"></div>
+	              	</div>
+	              	<div class="widget_summary">
+		                <div class="w_left w_25">
+		                  <span>Pinjam</span>
+		                </div>
+		                <div class="w_center w_55">
+		                  <div class="progress">
+		                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 2%;">
+		                      <span class="sr-only">60% Complete</span>
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="w_right w_20">
+		                  <span>1k</span>
+		                </div>
+		                <div class="clearfix"></div>
+	              	</div>
+	              	<div class="widget_summary">
+		                <div class="w_left w_25">
+		                  <span>Akta Hibah</span>
+		                </div>
+		                <div class="w_center w_55">
+		                  <div class="progress">
+		                    <div class="progress-bar bg-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 2%;">
+		                      <span class="sr-only">60% Complete</span>
+		                    </div>
+		                  </div>
+		                </div>
+		                <div class="w_right w_20">
+		                  <span>1k</span>
+		                </div>
+		                <div class="clearfix"></div>
+	              	</div>
+	            </div>
+	      	</div>
+        </div>
+    </div>
+</div>
+
+
+<?php
+$this->load->view('layout/footer');
+?>
