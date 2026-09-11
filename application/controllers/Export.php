3d9f148a4e90a8f9d1e38c5664fe94bc8b189224
Andreas Febrian I <44549277+Andreash07@users.noreply.github.com>
2026-09-11 22:41:24 +0700

    test3

--- application/controllers/Export.php
+++ application/controllers/Export.php
@@ -122,7 +122,7 @@ class Export extends CI_Controller {
 		//die('asdas');
 	    #$url = 'http://localhost:800/asset_gkp/export/dashboard_pdf';
 	    if(strpos(base_url(), ':') !== FALSE){
-	    	$url = explode(':', base_url())[0]'/export/dashboard_pdf';
+	    	$url = explode(':', base_url())[0].'/export/dashboard_pdf';
 	    }else{
 	    	$url = base_url().'export/dashboard_pdf';
 	    }
